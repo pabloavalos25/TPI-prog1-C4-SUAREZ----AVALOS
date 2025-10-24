55c49e344f1e34d1fd87aa8a6dd63d63ec21796d
ismaelsuarez <ismaelmauriciosuarez@gmail.com>
2025-10-24 14:17:29 -0300

    Maquetando Menu Carpetas

--- app/main.py
+++ app/main.py
@@ -1,22 +1,25 @@
 
-from tools.validaciones import *
-
-
 def main():
     #Inicializa el csv
-    inicializar_archivo()
+    #inicializar_archivo()
     
     #Menu de opciones
     while True:
         try:
-            print("****Menu Principal****")
-            print("1- Mostrar productos")
-            print("2- Agregar un producto")
-            print("3- Eliminar un producto")
-            print("4- Actualizar precios")
-            print("5- Salir")
+            print("*****INFO GEOGRAFICO*****")
+            print("1. Buscar pais por nombre")
+            print("2. Filtrar por continente")
+            print("3. Filtrar por rango de poblacion")
+            print("4. Filtrar por rango de superficie")
+            print("5. Ordenar paises")
+            print("6. Mostrar estadisticas")
+            print("7. Agregar un pais")
+            print("8. Editar poblacion y superficie de un pais")
+            print("9. Salir")
                         
-            opcion=int(input("Ingrese una opcion 1-5"))
+            opcion=int(input("Ingrese una opcion 1-9: "))
+            print("************************")
+
             match opcion:
                 case 1:
                         pass
@@ -28,6 +31,15 @@ def main():
                         pass
                 case 5:
                         pass
+                case 6:
+                        pass
+                case 7:
+                        pass
+                case 8:
+                        pass
+                case 9:
+                        print("Saliendo del programa")
+                        break  
                 case _:
                     print("Opcion incorrecta")
         except ValueError:
